62fa3e0e5139ea3e345b46cede421e8eb8497e71
legend <karroumhoussam2020@gmail.com>
2025-05-19 10:54:40 +0100

    add client service

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Client } from '../model/client.model';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ClientService {
+  private apiUrl = '/api/clients';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Client[]> {
+    return this.http.get<Client[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Client> {
+    return this.http.get<Client>(`${this.apiUrl}/${id}`);
+  }
+
+  create(client: Client): Observable<Client> {
+    return this.http.post<Client>(this.apiUrl, client);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
